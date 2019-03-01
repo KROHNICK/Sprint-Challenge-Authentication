725318dcc865395cf26ca9d918638564155c958f
KROHNICK <business.kanekperez@gmail.com>
2019-03-01 09:05:45 -0800

    register client functional

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Route, NavLink, withRouter } from "react-router-dom";
+import { Button } from "reactstrap";
+import Register from "./components/register";
 
 class App extends Component {
+  logout = () => {
+    localStorage.removeItem("jwt");
+
+    this.props.history.push("/login");
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <>
+        <header>
+          <nav>
+            <NavLink to="/register">Register</NavLink>
+            &nbsp;|&nbsp;
+            <NavLink to="/login">Login</NavLink>
+            &nbsp;|&nbsp;
+            <NavLink to="/jokes">Jokes</NavLink>
+            &nbsp;|&nbsp;
+            <Button onClick={this.logout}>Log Out</Button>
+          </nav>
         </header>
-      </div>
+        <main>
+          <Route path="/register" component={Register} />
+          {/* <Route path="/login" component={Login} />
+          <Route path="/jokes" component={Jokes} /> */}
+        </main>
+      </>
     );
   }
 }
